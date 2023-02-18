37e4987f6af81d18dd0cc26dd00b0c82e0780d3c
Florian Plattner <64537872+lasernoises@users.noreply.github.com>
2023-02-18 13:00:27 +0100

    Initial commit

---
+++ src/app.rs
@@ -0,0 +1,354 @@
+use egui::{ScrollArea, Ui, Window};
+use serde::{Deserialize, Serialize};
+
+use crate::state::{self, Level};
+
+// TODO: Figure out correct value
+const ABILITY_BOOST_COUNT: u8 = 3;
+const FIRST_LEVEL_SKILL_TRAINING_COUNT: u8 = 4;
+const SKILL_TRAINING_COUNT: u8 = 1;
+
+/// We derive Deserialize/Serialize so we can persist app state on shutdown.
+#[derive(Default, serde::Deserialize, serde::Serialize)]
+#[serde(default)] // if we add new fields, give them default values when deserializing old state
+pub struct TemplateApp {
+    state: state::CharSheet,
+}
+
+impl TemplateApp {
+    /// Called once before the first frame.
+    pub fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // This is also where you can customize the look and feel of egui using
+        // `cc.egui_ctx.set_visuals` and `cc.egui_ctx.set_fonts`.
+
+        // Load previous app state (if any).
+        // Note that you must enable the `persistence` feature for this to work.
+        if let Some(storage) = cc.storage {
+            return eframe::get_value(storage, eframe::APP_KEY).unwrap_or_default();
+        }
+
+        Default::default()
+    }
+}
+
+impl eframe::App for TemplateApp {
+    /// Called by the frame work to save state before shutdown.
+    fn save(&mut self, storage: &mut dyn eframe::Storage) {
+        eframe::set_value(storage, eframe::APP_KEY, self);
+    }
+
+    /// Called each time the UI needs repainting, which may be many times per second.
+    /// Put your widgets into a `SidePanel`, `TopPanel`, `CentralPanel`, `Window` or `Area`.
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        let Self { ref mut state } = self;
+
+        state.compute();
+
+        // ctx.set_visuals(egui::Visuals::light());
+
+        egui::SidePanel::left("side_panel")
+            .exact_width(256.)
+            .resizable(false)
+            .show(ctx, |ui| {
+                ui.heading("Plan");
+
+                plan(ui, state);
+            });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
+            // The central panel the region left after adding TopPanel's and SidePanel's
+
+            basic_stats(ui, state);
+
+            skill_list(ui, &mut state.computed.skills);
+        });
+    }
+}
+
+fn basic_stats(ui: &mut Ui, sheet: &mut state::CharSheet) {
+    ui.vertical(|ui| {
+        ui.horizontal(|ui| {
+            ui.label("Level:");
+            egui::ComboBox::from_id_source("level")
+                .selected_text(format!("{}", sheet.level))
+                .show_ui(ui, |ui| {
+                    ui.selectable_value(&mut sheet.level, 1, "1");
+                    ui.selectable_value(&mut sheet.level, 2, "2");
+                    ui.selectable_value(&mut sheet.level, 3, "3");
+                    ui.selectable_value(&mut sheet.level, 4, "4");
+                    ui.selectable_value(&mut sheet.level, 5, "5");
+                    ui.selectable_value(&mut sheet.level, 6, "6");
+                    ui.selectable_value(&mut sheet.level, 7, "7");
+                    ui.selectable_value(&mut sheet.level, 8, "8");
+                    ui.selectable_value(&mut sheet.level, 9, "9");
+                    ui.selectable_value(&mut sheet.level, 10, "10");
+                    ui.selectable_value(&mut sheet.level, 11, "11");
+                    ui.selectable_value(&mut sheet.level, 12, "12");
+                    ui.selectable_value(&mut sheet.level, 13, "13");
+                    ui.selectable_value(&mut sheet.level, 14, "14");
+                    ui.selectable_value(&mut sheet.level, 15, "15");
+                    ui.selectable_value(&mut sheet.level, 16, "16");
+                    ui.selectable_value(&mut sheet.level, 17, "17");
+                    ui.selectable_value(&mut sheet.level, 18, "18");
+                    ui.selectable_value(&mut sheet.level, 19, "19");
+                    ui.selectable_value(&mut sheet.level, 20, "20");
+                });
+
+            ui.label("Character Name:");
+            ui.text_edit_singleline(&mut sheet.name);
+        });
+    });
+}
+
+macro_rules! skills {
+    ($((label: $label:expr, field: $field:ident, ability: $ability:ident,)),*,) => {
+        fn skill_list(ui: &mut Ui, skills: &mut Skills) {
+            egui::Grid::new("skill_list")
+                .num_columns(3)
+                .spacing([40.0, 4.0])
+                .striped(true)
+                .show(ui, |ui| {
+                    $(
+                        ui.label($label);
+                        ui.label(skills.$field.0.label());
+                        ui.label(format!("{:+}", skills.$field.1));
+                        ui.end_row();
+                    )*
+                });
+        }
+
+        #[derive(Clone, Debug, Default, Deserialize, Serialize)]
+        pub struct Skills {
+            $(
+                pub $field: (state::Training, i8),
+            )*
+        }
+
+        #[derive(Clone, Debug, Default, Deserialize, Serialize)]
+        pub struct SkillTraining {
+            $(
+                /// True means an increase of one.
+                pub $field: bool,
+            )*
+        }
+
+        impl Skills {
+            pub fn apply_training(&mut self, training: &SkillTraining) {
+                $(
+                    if (training.$field) {
+                        self.$field.0.increase();
+                    }
+                )*
+            }
+
+            pub fn compute_modifiers(&mut self, level: Level, abilities: &state::Abilities) {
+                $(
+                    self.$field.1 = abilities.$ability + self.$field.0.proficiency_bonus(level) as i8;
+                )*
+            }
+        }
+
+        fn skill_training(ui: &mut Ui, sheet: &mut state::CharSheet, level: Level) {
+            let level_plan = sheet.level_plan(level);
+            let training = &mut level_plan.skill_training;
+
+            let selected_count = 0 $(+ training.$field as u8)*;
+
+            let max_count = if level == 1 {
+                FIRST_LEVEL_SKILL_TRAINING_COUNT
+            } else {
+                SKILL_TRAINING_COUNT
+            };
+
+            ui.label(format!("{selected_count} / {max_count}"));
+
+            let full = selected_count >= max_count;
+
+            $(
+                ui.add_enabled(
+                    !full || training.$field,
+                    egui::Checkbox::new(&mut training.$field, $label),
+                );
+            )*
+        }
+    };
+}
+
+skills![
+    (
+        label: "Acrobatics",
+        field: acrobatics,
+        ability: dexterity,
+    ),
+    (
+        label: "Athletics",
+        field: athletics,
+        ability: strength,
+    ),
+    (
+        label: "Computers",
+        field: computers,
+        ability: mind,
+    ),
+    (
+        label: "Crafting",
+        field: crafting,
+        ability: mind,
+    ),
+    (
+        label: "Deception",
+        field: deception,
+        ability: charisma,
+    ),
+    (
+        label: "Diplomacy",
+        field: diplomacy,
+        ability: charisma,
+    ),
+    (
+        label: "Medicine",
+        field: medicine,
+        ability: mind,
+    ),
+    (
+        label: "Nature",
+        field: nature,
+        ability: mind,
+    ),
+    (
+        label: "Performance",
+        field: performance,
+        ability: charisma,
+    ),
+    (
+        label: "Piloting",
+        field: piloting,
+        ability: mind,
+    ),
+    (
+        label: "Society",
+        field: society,
+        ability: mind,
+    ),
+    (
+        label: "Stealth",
+        field: stealth,
+        ability: dexterity,
+    ),
+    (
+        label: "Survival",
+        field: survival,
+        ability: mind,
+    ),
+    (
+        label: "Thievery",
+        field: thievery,
+        ability: dexterity,
+    ),
+    (
+        label: "Intimidation",
+        field: intimidation,
+        ability: charisma,
+    ),
+];
+
+fn plan(ui: &mut Ui, sheet: &mut state::CharSheet) {
+    ScrollArea::vertical().show(ui, |ui| {
+        ui.with_layout(
+            egui::Layout {
+                main_dir: egui::Direction::TopDown,
+                main_wrap: true,
+                main_align: egui::Align::Min,
+                main_justify: false,
+                cross_align: egui::Align::Max,
+                cross_justify: true,
+            },
+            |ui| {
+                for l in 1..=20 {
+                    level(ui, sheet, l);
+                }
+            },
+        );
+    });
+}
+
+fn level(ui: &mut Ui, sheet: &mut state::CharSheet, level: Level) {
+    ui.push_id(level, |ui| {
+        ui.vertical(|ui| {
+            ui.label(format!("Level {level}"));
+
+            if [1, 5, 10, 15, 20].contains(&level) {
+                window_button("Abilities", ui, |ui| {
+                    ability_boosts(ui, sheet, level);
+                });
+            }
+
+            if [1, 3, 5, 7, 9, 11, 13, 15, 17, 19].contains(&level) {
+                window_button("Skill Training", ui, |ui| {
+                    skill_training(ui, sheet, level);
+                });
+            }
+
+            if level == 1 {
+                window_button("Heritage", ui, |ui| {});
+            }
+
+            window_button("Ancestry Feat", ui, |ui| {});
+            window_button("Class Feat", ui, |ui| {});
+
+            if level == 1 {
+                window_button("Cantrips", ui, |ui| {});
+            }
+
+            window_button("Spells", ui, |ui| {});
+        });
+    });
+}
+
+fn ability_boosts(ui: &mut Ui, sheet: &mut state::CharSheet, level: Level) {
+    let level_plan = sheet.level_plan(level);
+    let boosts = &mut level_plan.ability_boosts;
+
+    let selected_count =
+        boosts.strength as u8 + boosts.dexterity as u8 + boosts.mind as u8 + boosts.charisma as u8;
+
+    ui.label(format!("{selected_count} / {ABILITY_BOOST_COUNT}"));
+
+    let full = selected_count >= ABILITY_BOOST_COUNT;
+
+    ui.add_enabled(
+        !full || boosts.strength,
+        egui::Checkbox::new(&mut boosts.strength, "Strength"),
+    );
+    ui.add_enabled(
+        !full || boosts.dexterity,
+        egui::Checkbox::new(&mut boosts.dexterity, "Dexterity"),
+    );
+    ui.add_enabled(
+        !full || boosts.mind,
+        egui::Checkbox::new(&mut boosts.mind, "Mind"),
+    );
+    ui.add_enabled(
+        !full || boosts.charisma,
+        egui::Checkbox::new(&mut boosts.charisma, "Charisma"),
+    );
+}
+
+fn window_button(title: &str, ui: &mut Ui, add_contents: impl FnOnce(&mut Ui)) {
+    ui.push_id(title, |ui| {
+        let id = ui.id().with("open");
+        let mut open = ui.data_mut(|d| d.get_temp(id).unwrap_or(false));
+
+        if ui.button(title).clicked() {
+            open ^= true;
+        }
+
+        Window::new(title)
+            .id(ui.id().with("window"))
+            .open(&mut open)
+            .collapsible(false)
+            .show(ui.ctx(), add_contents);
+
+        ui.data_mut(|d| d.insert_temp(id, open));
+    });
+}
---
+++ src/state.rs
@@ -0,0 +1,219 @@
+use serde::{Deserialize, Serialize};
+
+// Maybe make a newtype from this.
+// With methods for the string and stuff.
+pub type Level = u8;
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct CharSheet {
+    pub name: String,
+
+    pub level: Level,
+    pub ethnicity: String,
+    pub species: Species,
+    pub size: u8, // in centimeters
+
+    pub class: Class,
+
+    pub plan: [LevelPlan; 20],
+
+    #[serde(skip)]
+    pub computed: Computed,
+}
+
+impl Default for CharSheet {
+    fn default() -> Self {
+        Self {
+            name: Default::default(),
+            level: 1,
+            ethnicity: Default::default(),
+            species: Default::default(),
+            size: 170,
+
+            class: Default::default(),
+
+            plan: Default::default(),
+
+            computed: Default::default(),
+        }
+    }
+}
+
+impl CharSheet {
+    // This should be cheap enough that it can be done in every frame.
+    pub fn compute(&mut self) {
+        self.computed = Default::default();
+
+        for l in 1..=self.level {
+            self.computed
+                .skills
+                .apply_training(&self.plan[(l - 1) as usize].skill_training);
+
+            self.computed
+                .abilities
+                .apply_boosts(&self.plan[(l - 1) as usize].ability_boosts)
+        }
+
+        self.computed
+            .skills
+            .compute_modifiers(self.level, &self.computed.abilities);
+    }
+
+    pub fn level_plan(&mut self, level: Level) -> &mut LevelPlan {
+        &mut self.plan[(level - 1) as usize]
+    }
+}
+
+#[derive(Clone, Debug, Default)]
+pub struct Computed {
+    pub fortitude: Training,
+    pub reflex: Training,
+    pub will: Training,
+
+    pub abilities: Abilities,
+    pub skills: crate::app::Skills,
+}
+
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct Abilities {
+    pub strength: i8,
+    pub dexterity: i8,
+    pub mind: i8,
+    pub charisma: i8,
+}
+
+impl Abilities {
+    pub fn apply_boosts(&mut self, boosts: &AbilityBoosts) {
+        self.strength += boosts.strength as i8;
+        self.dexterity += boosts.dexterity as i8;
+        self.mind += boosts.mind as i8;
+        self.charisma += boosts.charisma as i8;
+    }
+}
+
+#[derive(Copy, Clone, Debug, Default, Deserialize, Serialize)]
+#[non_exhaustive]
+pub enum Species {
+    #[default]
+    Human,
+}
+
+#[derive(Copy, Clone, Debug, Default, Deserialize, Serialize)]
+pub enum Gender {
+    #[default]
+    Female,
+    Male,
+}
+
+#[derive(Copy, Clone, Debug, Default, Deserialize, Serialize)]
+pub enum Training {
+    #[default]
+    None,
+    Trained,
+    Expert,
+    Master,
+    Legendary,
+}
+
+impl Training {
+    pub fn label(&self) -> &'static str {
+        use Training::*;
+
+        match self {
+            None => "U",
+            Trained => "T",
+            Expert => "E",
+            Master => "M",
+            Legendary => "L",
+        }
+    }
+
+    pub fn increase(&mut self) {
+        use Training::*;
+
+        *self = match self {
+            None => Trained,
+            Trained => Expert,
+            Expert => Master,
+            Master => Legendary,
+            Legendary => Legendary,
+        }
+    }
+
+    /// Pathfinder Core Rulebook Page 13:
+    /// Proficiency is a system that measures a character’s aptitude
+    /// at a specific task or quality, and it has five ranks: untrained,
+    /// trained, expert, master, and legendary. Proficiency gives
+    /// you a bonus that’s added when determining the following
+    /// modifiers and statistics: AC, attack rolls, Perception, saving
+    /// throws, skills, and the effectiveness of spells. If you’re
+    /// untrained, your proficiency bonus is +0. If you’re trained,
+    /// expert, master, or legendary, your proficiency bonus equals
+    /// your level plus 2, 4, 6, or 8, respectively.
+    pub fn proficiency_bonus(&self, level: Level) -> u8 {
+        use Training::*;
+
+        match self {
+            None => 0,
+            Trained => level + 2,
+            Expert => level + 4,
+            Master => level + 6,
+            Legendary => level + 8,
+        }
+    }
+}
+
+pub struct Inventory {
+    platinum: u32,
+    gold: u32,
+    silver: u32,
+    copper: u32,
+
+    items: Vec<(Item, u32)>,
+}
+
+pub struct Item {
+    id: u64,
+    name: String,
+}
+
+/// This contains all of the things a levelup can contain
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct LevelPlan {
+    pub skill_training: crate::app::SkillTraining,
+    pub ability_boosts: AbilityBoosts,
+
+    pub feats: u8,
+}
+
+// Could also be a bit field.
+#[derive(Clone, Debug, Default, Deserialize, Serialize)]
+pub struct AbilityBoosts {
+    pub strength: bool,
+    pub dexterity: bool,
+    pub mind: bool,
+    pub charisma: bool,
+}
+
+struct Feat {
+    id: u64,
+    name: &'static str,
+
+    description: &'static str,
+
+    class: Class,
+}
+
+#[derive(Copy, Clone, Debug, Default, Deserialize, Serialize)]
+pub enum Class {
+    #[default]
+    Engineer,
+    Scout,
+}
+
+const FEATS: &[Feat] = &[Feat {
+    id: 1,
+    name: "Quick Engineering",
+    description: "Bla bla bla",
+    class: Class::Engineer,
+}];
